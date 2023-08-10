6742e5cfc8af12916e80e00a1aab1f85207aaefc
Yashwant <84570757+Yashwant077@users.noreply.github.com>
2023-08-10 12:53:26 +0530

    Create 10-08-23-Longest_Common_Subsequence.cpp

---
+++ 10-08-23-Longest_Common_Subsequence.cpp
@@ -0,0 +1,55 @@
+/*
+Problem Description: Longest Common Subsequence
+
+Given two strings, find the length of longest subsequence present in both of them. Both the strings are in uppercase latin alphabets.
+
+Example 1:
+
+Input:
+A = 6, B = 6
+str1 = ABCDGH
+str2 = AEDFHR
+Output: 3
+Explanation: LCS for input strings “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+
+Example 2:
+
+Input:
+A = 3, B = 2
+str1 = ABC
+str2 = AC
+Output: 2
+Explanation: LCS of "ABC" and "AC" is "AC" of length 2.
+Your Task:
+Complete the function lcs() which takes the length of two strings respectively and two strings as input parameters and returns the length of the longest subsequence present in both of them.
+
+Expected Time Complexity : O(|str1|*|str2|)
+Expected Auxiliary Space: O(|str1|*|str2|)
+
+Constraints:
+1<=size(str1),size(str2)<=10^3
+
+*/
+
+
+int lcs(int n, int m, string s1, string s2)
+{
+        // your code here
+        int dp[n+1][m+1];
+        
+        for(int i=0; i<=n; i++)
+            for(int j=0; j<=m; j++)
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+                    
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[n][m];
+ }
