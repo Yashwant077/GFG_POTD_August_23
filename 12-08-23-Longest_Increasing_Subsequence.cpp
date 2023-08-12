fa43121ba93172733596c0cab233d129db9f4cf0
Yashwant <84570757+Yashwant077@users.noreply.github.com>
2023-08-12 14:59:39 +0530

    Create 12-08-23-Longest_Increasing_Subsequence.cpp

---
+++ 12-08-23-Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,61 @@
+/*
+Problem Description: Longest Increasing Subsequence
+
+Given an array of integers, find the length of the longest (strictly) increasing subsequence from the given array.
+
+Example 1:
+
+Input:
+N = 16
+A = {0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15}
+Output: 
+6
+Explanation:
+There are more than one LIS in this array. One such Longest increasing subsequence is {0,2,6,9,13,15}.
+
+Example 2:
+
+Input:
+N = 6
+A[] = {5,8,3,7,9,1}
+Output: 
+3
+Explanation:
+There are more than one LIS in this array.  One such Longest increasing subsequence is {5,7,9}.
+Your Task:
+Complete the function longestSubsequence() which takes the input array and its size as input parameters and returns the length of the longest increasing subsequence.
+
+Expected Time Complexity : O( N*log(N) )
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 ≤ N ≤ 10^4
+0 ≤ A[i] ≤ 10^6
+
+*/
+
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[])
+    {
+        // your code here
+        vector<int>ans;
+        ans.push_back(a[0]);
+        int length=1;
+        
+        for(int i=1; i<n; i++){
+            if(a[i]>ans.back()){
+                ans.push_back(a[i]);
+                length++;
+            }
+            else{
+                int index = lower_bound(ans.begin(),ans.end(),a[i])-ans.begin();
+                ans[index] = a[i];
+            }
+        }
+        
+        return length;
+    }
+};
